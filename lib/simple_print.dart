da8849aae1a8721c9d0af56bc2e02c5eee81384d
Charles Dyason <k4gcybex@gmail.com>
2025-01-28 09:52:46 +0200

    chore: formatting

--- lib/simple_print.dart
+++ lib/simple_print.dart
@@ -2,7 +2,7 @@ import 'package:flutter/foundation.dart';
 
 enum LogLevel { info, debug, warning, error }
 
-/// LoggerConfig class to store logging configuration. 
+/// LoggerConfig class to store logging configuration.
 class _LoggerConfig {
   static String? _appName;
 
@@ -18,14 +18,16 @@ void setAppTag(String appName) {
   _LoggerConfig.appName = appName;
 }
 
-/// Print a debug message to the console with the format as follows, `[LogLevel] [timestamp] [primary tag] [additionally provided tags] message`. 
+/// Print a debug message to the console with the format as follows, `[LogLevel] [timestamp] [primary tag] [additionally provided tags] message`.
 /// e.g. D 2021-09-29T14:00:00.000 [appName] [...tags] message
-/// 
+///
 /// The [toString()] function is called on the [message] object to convert to a string.
 /// If `debugOverride` is set to `false`, the message will not be printed.
-void printDebug(dynamic message, {String? tag, bool timestamp = true, bool debugOverride = kDebugMode}) {
+void printDebug(dynamic message,
+    {String? tag, bool timestamp = true, bool debugOverride = kDebugMode}) {
   if (debugOverride) {
-    _print(message.toString(), tag: tag, logLevel: LogLevel.debug, timestamp: timestamp);
+    _print(message.toString(),
+        tag: tag, logLevel: LogLevel.debug, timestamp: timestamp);
   }
 }
 
@@ -34,9 +36,11 @@ void printDebug(dynamic message, {String? tag, bool timestamp = true, bool debug
 ///
 /// The [toString()] function is called on the [message] object to convert to a string.
 /// If `debugOverride` is set to `false`, the message will not be printed.
-void printInfo(dynamic message, {String? tag, bool timestamp = true, bool debugOverride = kDebugMode}) {
+void printInfo(dynamic message,
+    {String? tag, bool timestamp = true, bool debugOverride = kDebugMode}) {
   if (debugOverride) {
-    _print(message.toString(), tag: tag, logLevel: LogLevel.info, timestamp: timestamp);
+    _print(message.toString(),
+        tag: tag, logLevel: LogLevel.info, timestamp: timestamp);
   }
 }
 
@@ -45,9 +49,11 @@ void printInfo(dynamic message, {String? tag, bool timestamp = true, bool debugO
 ///
 /// The [toString()] function is called on the [message] object to convert to a string.
 /// If `debugOverride` is set to `false`, the message will not be printed.
-void printWarning(dynamic message, {String? tag, bool timestamp = true, bool debugOverride = kDebugMode}) {
-  if(debugOverride) {
-    _print(message.toString(), tag: tag, logLevel: LogLevel.warning, timestamp: timestamp);
+void printWarning(dynamic message,
+    {String? tag, bool timestamp = true, bool debugOverride = kDebugMode}) {
+  if (debugOverride) {
+    _print(message.toString(),
+        tag: tag, logLevel: LogLevel.warning, timestamp: timestamp);
   }
 }
 
@@ -56,18 +62,24 @@ void printWarning(dynamic message, {String? tag, bool timestamp = true, bool deb
 ///
 /// The [toString()] function is called on the [message] object to convert to a string.
 /// If `debugOverride` is set to `false`, the message will not be printed.
-void printError(dynamic message, {String? tag, bool timestamp = true, bool debugOverride = kDebugMode}) {
-  if(debugOverride) {
-    _print(message.toString(), tag: tag, logLevel: LogLevel.error, timestamp: timestamp);
+void printError(dynamic message,
+    {String? tag, bool timestamp = true, bool debugOverride = kDebugMode}) {
+  if (debugOverride) {
+    _print(message.toString(),
+        tag: tag, logLevel: LogLevel.error, timestamp: timestamp);
   }
 }
 
 /// Print a message to the console with the format as follows, `[LogLevel] [timestamp] [primary tag] [additionally provided tags] message`.
-void _print(String message, {LogLevel logLevel = LogLevel.debug, String? tag, bool timestamp = true}) {
+void _print(String message,
+    {LogLevel logLevel = LogLevel.debug, String? tag, bool timestamp = true}) {
   // example of formatted log: W 2021-09-29T14:00:00.000 [web_callkit] ...[tags] message
   final type = logLevel.name[0].toUpperCase();
   final appTag = _LoggerConfig.appName;
-  final tags = [appTag, tag].where((element) => element != null).map((e) => "[$e]").join(" ");
+  final tags = [appTag, tag]
+      .where((element) => element != null)
+      .map((e) => "[$e]")
+      .join(" ");
   String time = "";
   if (timestamp) {
     time = DateTime.now().toIso8601String();
